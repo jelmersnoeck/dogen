7165bccb81546c9f0a87ded368e1751cee72abeb
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-06 21:57:54 +0100

    Template: add error handling functions to mock.

--- mocks/Template.go
+++ mocks/Template.go
@@ -7,6 +7,37 @@ type Template struct {
 	mock.Mock
 }
 
+func (_m *Template) AddError(err error) {
+	_m.Called(err)
+}
+func (_m *Template) Blocks() []pdf.Block {
+	ret := _m.Called()
+
+	var r0 []pdf.Block
+	if rf, ok := ret.Get(0).(func() []pdf.Block); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]pdf.Block)
+		}
+	}
+
+	return r0
+}
+func (_m *Template) Errors() []error {
+	ret := _m.Called()
+
+	var r0 []error
+	if rf, ok := ret.Get(0).(func() []error); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]error)
+		}
+	}
+
+	return r0
+}
 func (_m *Template) Layout() pdf.Layout {
 	ret := _m.Called()
 
@@ -33,18 +64,4 @@ func (_m *Template) LoadBlock(raw_block map[string]interface{}, raw_input map[st
 }
 func (_m *Template) LoadBlocks(user_input interface{}) {
 	_m.Called(user_input)
-}
-func (_m *Template) Blocks() []pdf.Block {
-	ret := _m.Called()
-
-	var r0 []pdf.Block
-	if rf, ok := ret.Get(0).(func() []pdf.Block); ok {
-		r0 = rf()
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]pdf.Block)
-		}
-	}
-
-	return r0
 }
