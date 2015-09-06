e46df0bb9dae04a767f790b1bb50d859d5f97dcf
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-06 17:15:59 +0100

    JsonTemplate: add tests.

--- pdf/json_template_test.go
+++ pdf/json_template_test.go
@@ -8,15 +8,30 @@ import (
 )
 
 func TestNewJsonTemplate(t *testing.T) {
-	bytes := []byte(`{
+	template, ok := pdf.NewJsonTemplate(inputBytes())
+
+	assert.NotNil(t, template, "Template can't be nil")
+	assert.True(t, ok, "Template not created properly")
+}
+
+func TestLayout(t *testing.T) {
+	template, _ := pdf.NewJsonTemplate(inputBytes())
+
+	layout := template.Layout()
+	assert.NotNil(t, layout, "Layout can't be nil")
+	assert.EqualValues(t, "L", layout.Orientation(), "Orientation is not correct")
+	assert.EqualValues(t, "mm", layout.Unit(), "Unit is not correct")
+	assert.EqualValues(t, 50, layout.Width(), "Width is not correct")
+	assert.EqualValues(t, 75, layout.Height(), "Height is not correct")
+}
+
+func inputBytes() []byte {
+	return []byte(`{
 		"layout": {
 			"orientation": "L",
 			"unit": "mm",
 			"width": 50,
-			"height": 50
+			"height": 75
 		}
 	}`)
-	template, ok := pdf.NewJsonTemplate(bytes)
-	assert.True(t, ok)
-	assert.NotNil(t, template.Layout())
 }
