43cdfdec761cf533de2f2091c583273198b812e7
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-06 21:49:43 +0100

    Template: add error functions to interface.

--- pdf/template.go
+++ pdf/template.go
@@ -1,8 +1,10 @@
 package pdf
 
 type Template interface {
+	AddError(err error)
+	Blocks() []Block
+	Errors() []error
 	Layout() Layout
 	LoadBlock(raw_block, raw_input map[string]interface{}) Block
 	LoadBlocks(user_input interface{})
-	Blocks() []Block
 }
