307fce24eb07a38632beabd6b71c8142e4c04a3d
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-07 19:52:41 +0100

    Mocks: update PDF.

--- mocks/Pdf.go
+++ mocks/Pdf.go
@@ -1,21 +1,42 @@
 package mocks
 
-import "github.com/jelmersnoeck/noscito/pdf"
 import "github.com/stretchr/testify/mock"
 
 import "bytes"
 
-type MPdf struct {
+type Pdf struct {
 	mock.Mock
 }
 
-func (_m *MPdf) HttpImage(url string, x float64, y float64, w float64, h float64, flow bool, tp string, link int, linkStr string) {
+func (_m *Pdf) HttpImage(url string, x float64, y float64, w float64, h float64, flow bool, tp string, link int, linkStr string) {
 	_m.Called(url, x, y, w, h, flow, tp, link, linkStr)
 }
-func (_m *MPdf) ParseBlocks(blocks []pdf.Block) {
-	_m.Called(blocks)
+func (_m *Pdf) Position() (float64, float64) {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	var r1 float64
+	if rf, ok := ret.Get(1).(func() float64); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Get(1).(float64)
+	}
+
+	return r0, r1
 }
-func (_m *MPdf) Bytes(buffer *bytes.Buffer) []byte {
+func (_m *Pdf) SetPosition(x float64, y float64) {
+	_m.Called(x, y)
+}
+func (_m *Pdf) Text(text string) {
+	_m.Called(text)
+}
+func (_m *Pdf) Bytes(buffer *bytes.Buffer) []byte {
 	ret := _m.Called(buffer)
 
 	var r0 []byte
