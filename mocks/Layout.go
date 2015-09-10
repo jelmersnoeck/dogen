9c90735e2cfa16360db633ac9cec08ea71621957
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-10 22:45:02 +0100

    pdf test: add more tests.

---
+++ mocks/Layout.go
@@ -0,0 +1,56 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+type Layout struct {
+	mock.Mock
+}
+
+func (_m *Layout) Orientation() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+func (_m *Layout) Unit() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+func (_m *Layout) Width() float64 {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	return r0
+}
+func (_m *Layout) Height() float64 {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	return r0
+}
--- pdf/gofpdf_test.go
+++ pdf/gofpdf_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/jelmersnoeck/noscito/pdf"
+	"github.com/jung-kurt/gofpdf"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -21,6 +22,20 @@ func (s *GoFpdfSuite) TestNewGoFpdf() {
 
 	var ep *pdf.GoFpdf
 	assert.IsType(s.T(), ep, easypdf)
+}
+
+func (s *GoFpdfSuite) TestDocument() {
+	easypdf := newPdf()
+
+	var doc *gofpdf.Fpdf
+	assert.IsType(s.T(), doc, easypdf.Document())
+}
+
+func (s *GoFpdfSuite) TestLayout() {
+	layout, _ := pdf.NewPageLayout("L", "mm", 15, 15)
+	fpdf := pdf.NewGoFpdf(layout)
+
+	assert.Equal(s.T(), layout, fpdf.Layout())
 }
 
 func newPdf() *pdf.GoFpdf {
