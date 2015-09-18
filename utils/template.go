2bfba748b71a2c5c4f5c372cba5676cb22b77de2
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-18 20:04:10 +0100

    Template: add LoadTemplate tests.

--- utils/template.go
+++ utils/template.go
@@ -1,26 +1,27 @@
 package utils
 
 import (
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"path"
 	"runtime"
 )
 
-func LoadTemplate(name string) []byte {
+var NonExistingTemplateError = errors.New("Template does not exist.")
+
+func LoadTemplate(name string) ([]byte, error) {
 	_, filename, _, ok := runtime.Caller(1)
 
 	if !ok {
-		return nil
+		return nil, errors.New("Could not find current path.")
 	}
 
 	filepath := path.Join(path.Dir(filename), "../templates/"+name+".json")
 	file, err := ioutil.ReadFile(filepath)
 
 	if err != nil {
-		fmt.Printf(err.Error())
-		return nil
+		return nil, NonExistingTemplateError
 	}
 
-	return file
+	return file, nil
 }
---
+++ utils/template_test.go
@@ -0,0 +1,33 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/jelmersnoeck/noscito/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TemplateUtilSuite struct {
+	suite.Suite
+}
+
+func TestTemplateUtilSuite(t *testing.T) {
+	suite.Run(t, new(TemplateUtilSuite))
+}
+
+func (s *TemplateUtilSuite) TestLoadExistingTemplate() {
+	template, err := utils.LoadTemplate("pb-collection")
+
+	assert.Nil(s.T(), err)
+
+	var byt []byte
+	assert.IsType(s.T(), byt, template)
+}
+
+func (s *TemplateUtilSuite) TestLoadNonExistingTemplate() {
+	template, err := utils.LoadTemplate("non-existing")
+
+	assert.Equal(s.T(), utils.NonExistingTemplateError, err)
+	assert.Nil(s.T(), template)
+}
