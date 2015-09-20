4e70436a6c368a27ca48219301bc812570822a0d
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-20 19:24:11 +0100

    TextBox: proper positioning.

--- pdf/text_box.go
+++ pdf/text_box.go
@@ -1,22 +1,70 @@
 package pdf
 
+import "github.com/jelmersnoeck/noscito/utils"
+
 // TextBox takes a string of text and puts it on the given position on the page.
 type TextBox struct {
-	Text string  `mapstructure:"text"`
-	X    float64 `mapstructure:"x"`
-	Y    float64 `mapstructure:"y"`
+	Text       string   `mapstructure:"text"`
+	HTML       string   `mapstructure:"html"`
+	Size       float64  `mapstructure:"size"`
+	Color      string   `mapstructure:"color"`
+	Width      float64  `mapstructure:"width"`
+	Fill       bool     `mapstructure:"fill"`
+	Align      string   `mapstructure:"align"`
+	LineHeight float64  `mapstructure:"lineheight"`
+	Position   Position `mapstructure:"position"`
 }
 
 // Parse puts the text on a specific position on the page.
 func (b *TextBox) Parse(doc Document) {
-	startX, startY := doc.GetXY()
+	doc.AddFont("ProximaNova", "", "proximanova-regular-webfont.json")
+	doc.AddFont("ProximaNova", "B", "proximanova-bold-webfont.json")
 
-	doc.SetXY(b.X, b.Y)
-	doc.MultiCell(0, 5, b.Text, "", "", false)
-	doc.SetXY(startX, startY)
+	doc.SetTextColor(utils.HexToRGB(b.Color))
+	doc.SetFont("ProximaNova", "", b.Size)
+
+	if b.Text != "" {
+		b.setPosition(doc)
+		doc.MultiCell(b.Width, doc.PointConvert(b.LineHeight), b.Text, "", b.Align, b.Fill)
+	}
+
+	if b.HTML != "" {
+		leftMargin, _, rightMargin, _ := doc.GetMargins()
+		doc.SetLeftMargin(b.Position.X)
+		doc.SetY(b.Position.Y)
+
+		if b.Width > 0 {
+			pageWidth, _ := doc.GetPageSize()
+			rm := pageWidth - (b.Position.X + b.Width)
+			doc.SetRightMargin(rm)
+		}
+
+		html := doc.HTMLBasicNew()
+		html.Write(doc.PointConvert(b.LineHeight), b.HTML)
+
+		doc.SetLeftMargin(leftMargin)
+		doc.SetRightMargin(rightMargin)
+	}
 }
 
 // Load sets all the options for the text block like transformation, font,
 // color, etc.
-func (i *TextBox) Load(t Template, block_data, user_input map[string]interface{}) {
+func (b *TextBox) Load(t Template, block_data, user_input map[string]interface{}) {
+	if block_data["align"] == nil {
+		b.Align = "CM"
+	}
+
+	if block_data["lineheight"] == nil {
+		b.LineHeight = b.Size + 2
+	}
+}
+
+func (b *TextBox) setPosition(doc Document) {
+	if b.Position.Y >= 0 {
+		doc.SetY(b.Position.Y)
+	}
+
+	if b.Position.X >= 0 {
+		doc.SetX(b.Position.X)
+	}
 }
--- pdf/text_box_test.go
+++ pdf/text_box_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jelmersnoeck/noscito/mocks"
 	"github.com/jelmersnoeck/noscito/pdf"
+	"github.com/jung-kurt/gofpdf"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -16,16 +17,65 @@ func TestTextBoxBlockSuite(t *testing.T) {
 	suite.Run(t, new(TextBoxBlockSuite))
 }
 
-func (s *TextBoxBlockSuite) TestPositioning() {
-	doc := &mocks.Document{}
-	txtBx := &pdf.TextBox{"Text", 5.5, 5.5}
-
-	doc.On("GetXY").Return(10.5, 10.5)
-	doc.On("SetXY", 5.5, 5.5).Return(true)
-	doc.On("SetXY", 10.5, 10.5).Return(true)
-	doc.On("MultiCell", 0.0, 5.0, txtBx.Text, "", "", false).Return(true)
+func (s *TextBoxBlockSuite) TestFontSettings() {
+	doc, txtBx := setupDocument("", "")
 
 	txtBx.Parse(doc)
 
 	doc.AssertExpectations(s.T())
+}
+
+func (s *TextBoxBlockSuite) TestTextParsing() {
+	doc, txtBx := setupDocument("Test document", "")
+
+	doc.On("SetY", 10.0).Return(true)
+	doc.On("SetX", 5.0).Return(true)
+	doc.On("PointConvert", 12.0).Return(20.0)
+	doc.On("MultiCell", 5.5, 20.0, "Test document", "", "C", false).Return(true)
+
+	txtBx.Parse(doc)
+
+	doc.AssertExpectations(s.T())
+}
+
+func (s *TextBoxBlockSuite) TestHTMLParsing() {
+	doc, txtBx := setupDocument("", "HTML Document")
+	pdf := gofpdf.New("P", "mm", "A4", "")
+
+	doc.On("GetMargins").Return(1.0, 2.0, 3.0, 4.0)
+	doc.On("GetPageSize").Return(20.0, 30.0)
+	doc.On("PointConvert", 12.0).Return(20.0)
+
+	doc.On("SetLeftMargin", 5.0).Return(true)
+	doc.On("SetLeftMargin", 1.0).Return(true)
+	doc.On("SetRightMargin", 9.5).Return(true)
+	doc.On("SetRightMargin", 3.0).Return(true)
+	doc.On("SetY", 10.0).Return(true)
+	doc.On("HTMLBasicNew").Return(pdf.HTMLBasicNew())
+
+	txtBx.Parse(doc)
+
+	doc.AssertExpectations(s.T())
+}
+
+func setupDocument(txtStr, htmlStr string) (*mocks.Document, *pdf.TextBox) {
+	doc := &mocks.Document{}
+	txtBx := &pdf.TextBox{
+		txtStr,
+		htmlStr,
+		12,
+		"ffffff",
+		5.5,
+		false,
+		"C",
+		12,
+		pdf.Position{5, 10},
+	}
+
+	doc.On("AddFont", "ProximaNova", "", "proximanova-regular-webfont.json").Return(true)
+	doc.On("AddFont", "ProximaNova", "B", "proximanova-bold-webfont.json").Return(true)
+	doc.On("SetTextColor", 255, 255, 255).Return(true)
+	doc.On("SetFont", "ProximaNova", "", 12.0).Return(true)
+
+	return doc, txtBx
 }
