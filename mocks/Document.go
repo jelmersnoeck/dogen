c9ea0462bd432e2c27b7e49fb0e8ee092b496825
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-20 17:16:51 +0100

    Document: add more methods.

--- mocks/Document.go
+++ mocks/Document.go
@@ -9,6 +9,12 @@ type Document struct {
 	mock.Mock
 }
 
+func (_m *Document) AddFont(familyStr string, styleStr string, fileStr string) {
+	_m.Called(familyStr, styleStr, fileStr)
+}
+func (_m *Document) CellFormat(w float64, h float64, txtStr string, borderStr string, ln int, alignStr string, fill bool, link int, linkStr string) {
+	_m.Called(w, h, txtStr, borderStr, ln, alignStr, fill, link, linkStr)
+}
 func (_m *Document) Error() error {
 	ret := _m.Called()
 
@@ -33,6 +39,32 @@ func (_m *Document) GetConversionRatio() float64 {
 
 	return r0
 }
+func (_m *Document) GetFillColor() (int, int, int) {
+	ret := _m.Called()
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func() int); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 int
+	if rf, ok := ret.Get(1).(func() int); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Get(1).(int)
+	}
+
+	var r2 int
+	if rf, ok := ret.Get(2).(func() int); ok {
+		r2 = rf()
+	} else {
+		r2 = ret.Get(2).(int)
+	}
+
+	return r0, r1, r2
+}
 func (_m *Document) GetImageInfo(imageStr string) *gofpdf.ImageInfoType {
 	ret := _m.Called(imageStr)
 
@@ -43,6 +75,18 @@ func (_m *Document) GetImageInfo(imageStr string) *gofpdf.ImageInfoType {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*gofpdf.ImageInfoType)
 		}
+	}
+
+	return r0
+}
+func (_m *Document) GetMargins() float64 {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
 	}
 
 	return r0
@@ -66,6 +110,18 @@ func (_m *Document) GetXY() (float64, float64) {
 
 	return r0, r1
 }
+func (_m *Document) HTMLBasicNew() gofpdf.HTMLBasicType {
+	ret := _m.Called()
+
+	var r0 gofpdf.HTMLBasicType
+	if rf, ok := ret.Get(0).(func() gofpdf.HTMLBasicType); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(gofpdf.HTMLBasicType)
+	}
+
+	return r0
+}
 func (_m *Document) Image(imageNameStr string, x float64, y float64, w float64, h float64, flow bool, tp string, link int, linkStr string) {
 	_m.Called(imageNameStr, x, y, w, h, flow, tp, link, linkStr)
 }
@@ -81,8 +137,29 @@ func (_m *Document) ImageTypeFromMime(mimeStr string) string {
 
 	return r0
 }
+func (_m *Document) Line(x1 float64, y1 float64, x2 float64, y2 float64) {
+	_m.Called(x1, y1, x2, y2)
+}
+func (_m *Document) LineTo(x float64, y float64) {
+	_m.Called(x, y)
+}
 func (_m *Document) MultiCell(w float64, h float64, txtStr string, borderStr string, alignStr string, fill bool) {
 	_m.Called(w, h, txtStr, borderStr, alignStr, fill)
+}
+func (_m *Document) PointConvert(pt float64) float64 {
+	ret := _m.Called(pt)
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func(float64) float64); ok {
+		r0 = rf(pt)
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	return r0
+}
+func (_m *Document) Rect(x float64, y float64, w float64, h float64, styleStr string) {
+	_m.Called(x, y, w, h, styleStr)
 }
 func (_m *Document) RegisterImageReader(imgName string, tp string, r io.Reader) *gofpdf.ImageInfoType {
 	ret := _m.Called(imgName, tp, r)
@@ -98,9 +175,42 @@ func (_m *Document) RegisterImageReader(imgName string, tp string, r io.Reader) 
 
 	return r0
 }
-func (_m *Document) SetXY(x float64, y float64) {
-	_m.Called(x, y)
+func (_m *Document) SetCellMargin(margin float64) {
+	_m.Called(margin)
+}
+func (_m *Document) SetDrawColor(r int, g int, b int) {
+	_m.Called(r, g, b)
 }
 func (_m *Document) SetError(err error) {
 	_m.Called(err)
+}
+func (_m *Document) SetFillColor(r int, g int, b int) {
+	_m.Called(r, g, b)
+}
+func (_m *Document) SetFont(familyStr string, styleStr string, size float64) {
+	_m.Called(familyStr, styleStr, size)
+}
+func (_m *Document) SetLeftMargin(margin float64) {
+	_m.Called(margin)
+}
+func (_m *Document) SetRightMargin(margin float64) {
+	_m.Called(margin)
+}
+func (_m *Document) SetTextColor(r int, g int, b int) {
+	_m.Called(r, g, b)
+}
+func (_m *Document) SetTopMargin(margin float64) {
+	_m.Called(margin)
+}
+func (_m *Document) SetXY(x float64, y float64) {
+	_m.Called(x, y)
+}
+func (_m *Document) SetX(x float64) {
+	_m.Called(x)
+}
+func (_m *Document) SetY(y float64) {
+	_m.Called(y)
+}
+func (_m *Document) Write(h float64, txtStr string) {
+	_m.Called(h, txtStr)
 }
--- pdf/document.go
+++ pdf/document.go
@@ -10,14 +10,34 @@ import (
 // PDF document and draw to the file. This will usually be an external library
 // and just serves as an interface to pass along in our own methods.
 type Document interface {
+	AddFont(familyStr, styleStr, fileStr string)
+	CellFormat(w, h float64, txtStr string, borderStr string, ln int, alignStr string, fill bool, link int, linkStr string)
 	Error() error
 	GetConversionRatio() float64
+	GetFillColor() (int, int, int)
 	GetImageInfo(imageStr string) *gofpdf.ImageInfoType
+	GetMargins() (left, top, right, bottom float64)
 	GetXY() (float64, float64)
+	HTMLBasicNew() (html gofpdf.HTMLBasicType)
 	Image(imageNameStr string, x, y, w, h float64, flow bool, tp string, link int, linkStr string)
 	ImageTypeFromMime(mimeStr string) string
+	Line(x1, y1, x2, y2 float64)
+	LineTo(x, y float64)
 	MultiCell(w, h float64, txtStr, borderStr, alignStr string, fill bool)
+	PointConvert(pt float64) (u float64)
+	Rect(x, y, w, h float64, styleStr string)
 	RegisterImageReader(imgName, tp string, r io.Reader) *gofpdf.ImageInfoType
-	SetXY(x, y float64)
+	SetCellMargin(margin float64)
+	SetDrawColor(r, g, b int)
 	SetError(err error)
+	SetFillColor(r, g, b int)
+	SetFont(familyStr, styleStr string, size float64)
+	SetLeftMargin(margin float64)
+	SetRightMargin(margin float64)
+	SetTextColor(r, g, b int)
+	SetTopMargin(margin float64)
+	SetXY(x, y float64)
+	SetX(x float64)
+	SetY(y float64)
+	Write(h float64, txtStr string)
 }
