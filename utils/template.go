a04b7ab1dc8f1a8a7a913c6bdece34daef6e508c
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-20 19:59:43 +0100

    Template: test out heroku.

--- utils/template.go
+++ utils/template.go
@@ -5,19 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path"
-	"runtime"
 )
 
 var NonExistingTemplateError = errors.New("Template does not exist.")
 
 func LoadTemplate(name string) ([]byte, error) {
-	_, filename, _, ok := runtime.Caller(1)
-
-	if !ok {
-		return nil, errors.New("Could not find current path.")
-	}
-
-	filepath := path.Join(path.Dir(filename), "../templates/"+name+".json")
+	filepath := path.Join("/app/templates/" + name + ".json")
 	fmt.Println(filepath)
 	file, err := ioutil.ReadFile(filepath)
 
