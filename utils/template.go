fb99ed1f1fd28af849bddfab800a9f59ffbbb4e4
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-09-20 20:02:14 +0100

    TEmplate: correct base path.

--- utils/template.go
+++ utils/template.go
@@ -2,16 +2,15 @@ package utils
 
 import (
 	"errors"
-	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
 )
 
 var NonExistingTemplateError = errors.New("Template does not exist.")
 
 func LoadTemplate(name string) ([]byte, error) {
-	filepath := path.Join("/app/templates/" + name + ".json")
-	fmt.Println(filepath)
+	filepath := path.Join(os.Getwd() + "/templates/" + name + ".json")
 	file, err := ioutil.ReadFile(filepath)
 
 	if err != nil {
